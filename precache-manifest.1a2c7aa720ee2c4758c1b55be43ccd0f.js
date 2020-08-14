e7ef03a028b15db583c6bed3d7fafe4c6853a672
Roman <65417719+RomanBaukin@users.noreply.github.com>
2020-08-14 23:37:33 +0400

    Add files via upload

---
+++ precache-manifest.1a2c7aa720ee2c4758c1b55be43ccd0f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec49f3d361fa7b534d90bcf570c9c90f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2deaf46c40fb40f50e24",
+    "url": "/static/css/main.9e034465.chunk.css"
+  },
+  {
+    "revision": "ab12b72ec6cd5010e57a",
+    "url": "/static/js/2.42df7b18.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.42df7b18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2deaf46c40fb40f50e24",
+    "url": "/static/js/main.9bf34cb8.chunk.js"
+  },
+  {
+    "revision": "c0ccae055cb2c07e921a",
+    "url": "/static/js/runtime-main.4483ebb2.js"
+  }
+]);
